ddbf749cdd35379c16d91c2c3b3e8828f08b2dbf
Olivier Adams <oladams@fu-c3-p3.s19.be>
2024-10-29 15:51:48 +0100

    Client class

---
+++ srcs/network/Client.hpp
@@ -0,0 +1,29 @@
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+class Client {
+	public:
+		Client(int sock, &Server server);
+
+		int getSock(void) const;
+		time_t getLastActiveTime(void) const;
+		char* getWriteBuffer(void);
+
+		void updateLastActiveTime(void);
+		void appendReadBuffer(char &readBuffer[BUFFER_SIZE], int received);
+		void appendWriteBuffer(std::string response);
+		void appendWriteBuffer(char *response);
+
+		void clearReadBuffer(void);
+		void clearWriteBuffer(void);
+
+	private:
+		int _sock;
+		Server _server;
+		std::vector<char> _readBuffer;
+		std::vector<char> _writeBuffer;
+		time_t _lastActiveTime;
+};
+
+
+#endif
