efc589e769d5c15ea573a9003f6758ba14ce925c
Alexis Philippon <alexphil@student.s19.be>
2024-10-30 13:39:48 +0100

    Config.hpp

--- srcs/Configuration/Config.hpp
+++ srcs/Configuration/Config.hpp
@@ -6,7 +6,54 @@
 /*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 13:38:01 by alexphil          #+#    #+#             */
-/*   Updated: 2024/10/30 13:38:01 by alexphil         ###   ########.fr       */
+/*   Updated: 2024/10/30 13:39:35 by alexphil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# pragma once
+
+# include <string>
+# include <set>
+# include <map>
+# include <vector>
+# include <fstream>
+# include <sstream>
+
+# include "ServerConfig.hpp"
+# include "RouteConfig.hpp"
+
+class	Config {
+	public:
+		Config(int ac, char **av);
+		
+		// Methods
+		
+		bool		hasExtension(std::string filename, std::string extension);
+		bool		isDirective(std::string token);
+		bool		isRule(std::string token);
+		bool		indentLevel(size_t level);
+		bool		isCommentLine(std::string line);
+		
+		void		processDirective(int port, std::vector <std::string> tokens);
+		void		processRule(int port, std::string route, std::vector <std::string> tokens);	
+		
+		// Setters
+
+		void		addPort(int port);
+		void		addServer(int port, ServerConfig serverConfig);
+		
+		// Getters
+
+		std::set	<int>					getPorts();
+		std::map 	<int, ServerConfig>		getServers();
+	
+	private:
+		std::ifstream						configFile;
+		std::string							line;
+		
+		std::set 	<std::string>			directives;
+		std::set 	<std::string>			rules;
+		
+		std::set	<int>					usedPorts;
+		std::map	<int, ServerConfig>		serverConfigs;
+};
